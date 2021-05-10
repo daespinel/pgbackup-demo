804c6c408f05688a314e62eb8500f670d6934ab3
daespinel <dafespinelsa@unal.edu.co>
2021-05-10 16:54:36 +0200

    Implementing S3 interactions

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -1,4 +1,13 @@
+"""
+    Class defining the drivers for pg backup
+    local driver: provides the backup locally
+    s3 driver: Uses an S3 bucket for the backup
+"""
+
 def local(infile, outfile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+def s3(client, infile, bucket, name):
+    client.upload_fileobj(infile, bucket, name)
